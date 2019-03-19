9d572fa82122b2f61e0629accafdd8069e54f942
sora1 <shinnosuke960801@gmail.com>
2019-03-19 17:48:10 +0900

    Finish_function

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -6,19 +6,32 @@ describe ProductsController, type: :controller do
   let(:category) {product.third_category}
 
   describe 'GET #index' do
-    it "renders the :index template" do
+    before do
       get :index
+    end
+    it "renders the :index template" do
       expect(response).to render_template :index
     end
     it "assigns the requested categories to @categories" do
       create_list(:category, 7)
-      get :index
       expect(assigns(:categories).size).to eq 3
     end
     it "assigns the requested categories to @brands" do
        create_list(:brand, 7)
-       get :index
        expect(assigns(:brands).size).to eq 4
+    end
+  end
+
+  describe 'GET #new' do
+    before do
+      login user
+      get :new
+    end
+    it "renders the :new template" do
+      expect(response).to render_template :new
+    end
+    it "assigns @product" do
+      expect(assigns(:product)).to be_a_new Product
     end
   end
 
