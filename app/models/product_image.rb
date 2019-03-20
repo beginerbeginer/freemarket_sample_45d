b08b80501df74db59bbc4579e94b56c5cdb1b124
sora1 <shinnosuke960801@gmail.com>
2019-03-20 21:46:11 +0900

    fixed_something_for_product_images

--- app/models/product_image.rb
+++ app/models/product_image.rb
@@ -2,6 +2,4 @@ class ProductImage < ApplicationRecord
   belongs_to :product, optional: true
   validates :image, presence: true
   mount_uploader :image, ImageUploader
-
-  validates :image, presence: true
 end
